b67377703ad07f5d193c8b53bddce58009bea10e
Mac Ignacio <mack_ignacio@yahoo.com>
2019-10-19 13:19:25 +0800

    Fix #11 - Add InkHTML class

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const InkHTML = {
-  script: `setTimeout(()=>{ window.print(); window.close();},500)`,
-  html: (head, body) => `<!DOCTYPE html><html><head>${head}/head><body>${body}<script>${InkHTML.script}</script></body></html>`,
-  print: (body, options) => {
-    const defaulWidthHeight = ` width=${screen.availWidth},height=${screen.availHeight}`;
-    const defaultOptions = `menubar=no,toolbar=no,location=no,status=no,scrollbars=no,resizable=no,dependent,fullscreen=yes,${defaulWidthHeight}`;
-    const mapElements = ["input", "textarea", "select"].map(type => InkHTML.map[type](element));
-    const WinPrint = window.open("", "", options || defaultOptions);
-    WinPrint.document.write(InkHTML.html(document.head.innerHTML, body.innerHTML)).close();
-    WinPrint.focus();
-  },
-  map: {
-    map: (element, elementName, callback) => Array.prototype.slice.call(element.querySelectorAll(elementName)).map(callback),
-    mapSelected: element => (element.selected ? element.setAttribute("selected", "selected") : element),
-    isInputElement: el => ["checkbox", "radio"].includes(el.type) && el.checked,
-    setInputElement: el => (InkHTML.map.isInputElement(el) ? el.setAttribute("checked", "") : el.setAttribute("value", el.value)),
-    input: element => InkHTML.map(element, "input", el => InkHTML.map.setInputElement(el)),
-    textarea: element => InkHTML.map(element, "textarea", el => (el.innerHTML = el.value)),
-    select: element => InkHTML.map(element, "select", el => el.map(InkHTML.map.mapSelected)),
-  },
-};
+class InkHTML {
+  constructor() {
+    this.map = {
+      input: element => this.mapElement(element, "input", el => this.setInputElement(el)),
+      textarea: element => this.mapElement(element, "textarea", el => (el.innerHTML = el.value)),
+      select: element => this.mapElement(element, "select", el => el.map(this.mapSelected)),
+    };
+  }
 
-export default InkHTML.print;
+  script() {
+    return `setTimeout(()=>{ window.print(); window.close();},500)`;
+  }
+
+  html(head, body) {
+    return `<!DOCTYPE html><html><head>${head}</head><body>${body}<script>${this.script()}</script></body></html>`;
+  }
+
+  print(body, options) {
+    const defaultWidthHeight = `width=${screen.availWidth},height=${screen.availHeight}`;
+    const defaultOptions = `menubar=no,toolbar=no,location=no,status=no,scrollbars=no,resizable=no,dependent,fullscreen=yes,${defaultWidthHeight}`;
+    const mapElements = Object.keys(this.map).map(type => this.map[type](element));
+    const WinPrint = window.open("", "", options || defaultOptions);
+    WinPrint.document.write(this.html(document.head.innerHTML, body.innerHTML)).close();
+    WinPrint.focus();
+  }
+
+  mapElement(element, elementName, callback) {
+    return Array.prototype.slice.call(element.querySelectorAll(elementName)).map(callback);
+  }
+
+  mapSelected(element) {
+    return element.selected ? element.setAttribute("selected", "selected") : element;
+  }
+
+  isInputElement(element) {
+    return ["checkbox", "radio"].includes(element.type) && element.checked;
+  }
+
+  setInputElement(element) {
+    this.isInputElement(element) ? element.setAttribute("checked", "") : element.setAttribute("value", element.value);
+  }
+}
+
+const app = new InkHTML();
+export default app.print;
