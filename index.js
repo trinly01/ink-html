9e4cf0d62e122a94bed5e25be830c7d252c59606
Mac Ignacio <mack_ignacio@yahoo.com>
2019-10-19 12:57:55 +0800

    Add InkHTML object

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-const MapElement = {
-  map: (element, elementName, callback) => Array.prototype.slice.call(element.querySelectorAll(elementName)).map(callback),
-  mapSelected: (element) => element.selected ? element.setAttribute('selected', 'selected') : element,
-  isInputElement: (el) => ['checkbox', 'radio'].includes(el.type) && el.checked,
-  setInputElement: (el) => this.isInputElement(el) ? el.setAttribute('checked', '') : el.setAttribute('value', el.value),
-  input: (element) => this.map(element, "input", (el) => this.setInputElement(el)),
-  textarea: (element) => this.map(element, "textarea", (el) => el.innerHTML = el.value),
-  select: (element) => this.map(element, "select", (el) => el.map(this.mapSelected))
-}
+const InkHTML = {
+  script: `setTimeout(()=>{ window.print(); window.close();},500)`,
+  html: (head, body) => `<!DOCTYPE html><html><head>${head}/head><body>${body}<script>${InkHTML.script}</script></body></html>`,
+  print: (body, options) => {
+    const defaulWidthHeight = ` width=${screen.availWidth},height=${screen.availHeight}`;
+    const defaultOptions = `menubar=no,toolbar=no,location=no,status=no,scrollbars=no,resizable=no,dependent,fullscreen=yes,${defaulWidthHeight}`;
+    const mapElements = ["input", "textarea", "select"].map(type => InkHTML.map[type](element));
+    const WinPrint = window.open("", "", options || defaultOptions);
+    WinPrint.document.write(InkHTML.html(document.head.innerHTML, body.innerHTML)).close();
+    WinPrint.focus();
+  },
+  map: {
+    map: (element, elementName, callback) => Array.prototype.slice.call(element.querySelectorAll(elementName)).map(callback),
+    mapSelected: element => (element.selected ? element.setAttribute("selected", "selected") : element),
+    isInputElement: el => ["checkbox", "radio"].includes(el.type) && el.checked,
+    setInputElement: el => (InkHTML.map.isInputElement(el) ? el.setAttribute("checked", "") : el.setAttribute("value", el.value)),
+    input: element => InkHTML.map(element, "input", el => InkHTML.map.setInputElement(el)),
+    textarea: element => InkHTML.map(element, "textarea", el => (el.innerHTML = el.value)),
+    select: element => InkHTML.map(element, "select", el => el.map(InkHTML.map.mapSelected)),
+  },
+};
 
-function createHTML(head, body) {
-  return `<!DOCTYPE html><html>
-  <head>${head}/head>
-  <body>${body}<script>
-    setTimeout(()=>{ 
-        window.print()
-        window.close()
-      }, 500)</script>
-  </body></html>`;
-}
-
-function print(body, options) {
-  const WinPrint = window.open('', '', options)
-  WinPrint.document.write(createHTML(document.head.innerHTML, body))
-  WinPrint.document.close();
-  WinPrint.focus();
-}
-
-export default function (element, options) {
-  const defaultOptions = `menubar=no,toolbar=no,location=no,status=no,scrollbars=no,resizable=no,dependent,fullscreen=yes,width=${screen.availWidth},height=${screen.availHeight}`;
-  const mapElements = ["input", "textarea", "select"].map((type) => MapElement[type](element));
-  print(element.innerHTML, options || defaultOptions);
-}
+export default InkHTML.print;
